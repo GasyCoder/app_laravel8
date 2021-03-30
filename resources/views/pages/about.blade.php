0adb44e9b027fce6135ab9ec6fe32612b2b1f686
Florent Bezara <bezaraflorent@gmail.com>
2021-03-30 19:46:18 +0200

    Create new file app

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>About Us | Hello Madagascar</title>
-</head>
-<body>
+@extends('app')
+@section('content')
 <h1>Who are We?</h1>
 <p>Heur local {{ date('h:i:s A') }}.</p>
 <p>Create with &hearts; by GasyCoder</p>
 <p>Aller retour à l'accueil <a href="/">ici</a> </p>
-</body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,24 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Hello Madagascar</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-    <body>
+@extends('app')
+@section('content')
 <h1>Hello Malagasy</h1>
 <p>Heur local {{ date('h:i:s A') }}.</p>
 
-<footer>
-    &copy; Copyright {{ date('Y') }} &middot; <a href="/about-us">About Us</a>
-</footer>
-    </body>
-</html>
+@endsection
